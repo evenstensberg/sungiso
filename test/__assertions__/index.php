3fcdf8b38b0fa78221910b8217453db7cbb15669
ev1stensberg <evenstensberg@gmail.com>
2019-04-07 20:39:43 +0200

    chore: woo

---
+++ test/__assertions__/index.php
@@ -0,0 +1,29 @@
+{{-- Sweet --}}
+
+<!DOCTYPE html>
+<html class="no-js" lang="@yield('ayo')">
+
+	<head>
+	</head>
+
+	<body class="@isset ($:)) {{:)}} @endisset">
+		{{-- Google Tag Manager (noscript) --}}
+
+			<moo-aye></moo-aye>
+		@if (env('yeah') != 'megatrueorfalse')
+			<!--<3 -->
+			<script async >
+				(function() {
+					var asd = document.createElement('script');  asd.async = true;
+					lc.src = 'https://google.com/jquery.max.js';
+					var sa = document.getElementsByTagName('script')[0]; sa.parentNode.insertBefore(asd, sa);
+				})();
+			</script>
+			<!-- </3> -->
+		@endif
+
+    <script  src="{{mix('woo.js')}}"></script>
+	
+	</body>
+
+</html>
